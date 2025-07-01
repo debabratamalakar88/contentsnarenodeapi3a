0530bfafee0a3431943b45bebd815312e14b988f
Debabrata Malakar (Dev) <debabratamalakar88@gmail.com>
2025-07-01 07:30:44 +0000

    here I want to run this app via cpanel  to https://narlaxsoftware.com/de

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/dev/contentsnarefull/contentsnare',
   typescript: {
     ignoreBuildErrors: true,
   },
