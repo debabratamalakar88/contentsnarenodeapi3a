f453d9322d00d20d5e9f7428278a99e5aaaba764
Debabrata Malakar (Dev) <debabratamalakar88@gmail.com>
2025-07-01 06:36:32 +0000

    run this in current server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/dev/contentsnarefull/contentsnare',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
