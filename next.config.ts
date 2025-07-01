02ee356b7c62b27c24362984bcc959c0a165bf40
Debabrata Malakar (Dev) <debabratamalakar88@gmail.com>
2025-07-01 06:34:46 +0000

    If want to run this app in https://narlaxsoftware.com/dev/contentsnarefu

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  basePath: '/dev/contentsnarefull/contentsnare',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
