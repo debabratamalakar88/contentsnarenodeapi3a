f5a7a52faaee1cce9b26335e74247fae1379808e
Debabrata Malakar (Dev) <debabratamalakar88@gmail.com>
2025-07-01 08:00:51 +0000

    still give same error

--- src/app/dashboard/clients/[id]/edit/page.tsx
+++ src/app/dashboard/clients/[id]/edit/page.tsx
@@ -1,6 +1,8 @@
 import EditClientView from "./EditClientView";
 
 export async function generateStaticParams() {
+  // We don't need to pre-build any client edit pages.
+  // This will be handled client-side.
   return [];
 }
 
--- src/app/dashboard/clients/[id]/page.tsx
+++ src/app/dashboard/clients/[id]/page.tsx
@@ -2,6 +2,8 @@
 import ClientView from "./ClientView";
 
 export async function generateStaticParams() {
+    // We don't need to pre-build any client pages.
+    // This will be handled client-side.
     return [];
 }
 
