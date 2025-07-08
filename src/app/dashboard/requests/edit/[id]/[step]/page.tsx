dc8f2f537fa2ebadcf5272c5453e81416e139c96
Debabrata Malakar (Dev) <debabratamalakar88@gmail.com>
2025-07-08 10:49:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/requests/edit/[id]/[step]/page.tsx
+++ src/app/dashboard/requests/edit/[id]/[step]/page.tsx
@@ -1,7 +1,7 @@
 
 
 'use client'
-import { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useParams, useRouter } from "next/navigation";
 
 import StepNavigation from '../../../new/components/StepNavigation';
