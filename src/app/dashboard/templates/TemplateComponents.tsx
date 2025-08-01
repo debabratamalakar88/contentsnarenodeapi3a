cc878c63608028a7d7ee9299aee93fb12212383e
Debabrata Malakar (Dev) <debabratamalakar88@gmail.com>
2025-08-01 10:00:42 +0000

    sorry but still do not see any Delete link in the action tab

--- src/app/dashboard/templates/TemplateComponents.tsx
+++ src/app/dashboard/templates/TemplateComponents.tsx
@@ -201,12 +201,12 @@ export function MyTemplatesTable({ templates, currentUser, onDuplicate, onDelete
                             <>
                                 <DropdownMenuItem asChild><Link href={`/dashboard/templates/edit/${template.id}`}><Edit className="mr-2 h-4 w-4" />Edit</Link></DropdownMenuItem>
                                 <DropdownMenuItem onClick={() => onDuplicate(template.id)}><Copy className="mr-2 h-4 w-4" />Duplicate</DropdownMenuItem>
-                                {canDelete && (
-                                    <>
-                                        <DropdownMenuSeparator />
-                                        <DropdownMenuItem onClick={() => onDelete(template)} className="text-destructive focus:bg-destructive focus:text-destructive-foreground"><Trash2 className="mr-2 h-4 w-4" />Delete</DropdownMenuItem>
-                                    </>
-                                )}
+                            </>
+                        )}
+                        {canDelete && (
+                            <>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem onClick={() => onDelete(template)} className="text-destructive focus:bg-destructive focus:text-destructive-foreground"><Trash2 className="mr-2 h-4 w-4" />Delete</DropdownMenuItem>
                             </>
                         )}
                     </DropdownMenuContent>
