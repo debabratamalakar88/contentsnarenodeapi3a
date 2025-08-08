df0cae593bd309530ff00eb845eb7b5eab4e9682
Debabrata Malakar (Dev) <debabratamalakar88@gmail.com>
2025-08-08 07:28:52 +0000

    Calendar background should be white in calendar page in user panel

--- src/app/dashboard/calendar/page.tsx
+++ src/app/dashboard/calendar/page.tsx
@@ -5,7 +5,7 @@ import CalendarView from './CalendarView';
 
 export default function CalendarPage() {
     return (
-        <div className="flex flex-col h-full bg-background">
+        <div className="flex flex-col h-full bg-white">
             <CalendarView />
         </div>
     );
