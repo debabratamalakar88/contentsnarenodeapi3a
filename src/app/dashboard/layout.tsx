72bd8b778b274986cd5cdbcfbb7bfa0a6fe54613
Debabrata Malakar (Dev) <debabratamalakar88@gmail.com>
2025-11-26 14:59:56 +0000

    logo section min-width 200px

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -141,7 +141,7 @@ export default function DashboardLayout({
         <nav className="flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link
             href="/dashboard"
-            className="flex items-center gap-3 text-lg font-semibold md:text-base"
+            className="flex items-center gap-3 text-lg font-semibold md:text-base min-w-[200px]"
           >
             {company ? (
               <>
